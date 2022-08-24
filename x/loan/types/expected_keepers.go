ffa33ad08126b78fdd03df2228415b5f77ee96f3
crypto-ticky <developer613@sourceplay.com>
2022-08-24 02:10:26 -0700

    Add repay-loan message

--- x/loan/types/expected_keepers.go
+++ x/loan/types/expected_keepers.go
@@ -21,8 +21,15 @@ type AccountKeeper interface {
 //	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 //}
 
+//type BankKeeper interface {
+//	// Methods imported from bank should be defined here
+//	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
+//	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+//}
+
 type BankKeeper interface {
 	// Methods imported from bank should be defined here
 	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 }
